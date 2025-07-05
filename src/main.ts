cf96bcf5a853026e733cefec237ab4461537d3af
root <godofluck89@gmail.com>
2025-07-05 07:46:37 +0000

    fix

--- src/main.ts
+++ src/main.ts
@@ -14,22 +14,6 @@ const client = createPublicClient({
 
 const bot = new TelegramService()
 
-const contributed = {
-  address: POOL_ADDRESS as Address,
-  abi: parseAbi(["event Contributed(address indexed user, address currency, uint256 amount, uint256 volume, uint256 totalVolume)"]),
-  eventName: 'Contributed',
-  async onLogs (logs) {
-    for (const log of logs) {
-      const { user, amount } = log.args
-      if (!(user && amount)) continue
-      await bot.purchase({
-        wallet: user,
-        amount: formatUnits(amount, 18)
-      })
-    }
-  }
-}
-
 console.info(`Telegram Purchase Bot alert started!`)
 
 subscribe(client, {
